ade304abc647e92064416b1033d637c1ca2ebe7a
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2022-01-25 00:14:21 +0100

    feat: adding tailwindcss

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -27,7 +27,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -37,9 +37,11 @@ function App() {
             Vite Docs
           </a>
         </p>
+
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
