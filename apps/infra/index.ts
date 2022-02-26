1589c9118a6f41a67b990dcb44bcb36980b81cc6
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2022-02-26 19:34:35 +0100

    feat: adding infra folder

---
+++ apps/infra/index.ts
@@ -0,0 +1,42 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+const dynamoDbTable = new aws.dynamodb.Table("CEMDEV", {
+  name: "CEMDEV",
+  attributes: [
+    {
+      name: "PK",
+      type: "S",
+    },
+    {
+      name: "SK",
+      type: "S",
+    },
+  ],
+  hashKey: "PK",
+  rangeKey: "SK",
+  globalSecondaryIndexes: [
+    {
+      hashKey: "SK",
+      rangeKey: "PK",
+      name: "ReverseIndex",
+      projectionType: "ALL",
+      readCapacity: 10,
+      writeCapacity: 10,
+    },
+  ],
+  readCapacity: 20,
+  writeCapacity: 20,
+  billingMode: "PROVISIONED",
+  tags: {
+    Environment: "production",
+    Name: "CEMDEV",
+  },
+  ttl: {
+    attributeName: "ttl",
+    enabled: false,
+  },
+});
+
+export const dynamoDbTableArn = dynamoDbTable.arn;
