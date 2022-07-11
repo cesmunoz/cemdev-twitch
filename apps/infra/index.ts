abe966b1cc4e71d7b0a77f3eb28cc7b975cd2537
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2022-07-11 19:32:34 +0200

    fix: rcu wcu dynamodb tables

--- apps/infra/index.ts
+++ apps/infra/index.ts
@@ -1,80 +1,51 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-const dynamoDbTableProd = new aws.dynamodb.Table("CEMDEV", {
-  name: "CEMDEV",
-  attributes: [
-    {
-      name: "PK",
-      type: "S",
-    },
-    {
-      name: "SK",
-      type: "S",
-    },
-  ],
-  hashKey: "PK",
-  rangeKey: "SK",
-  globalSecondaryIndexes: [
-    {
-      hashKey: "SK",
-      rangeKey: "PK",
-      name: "ReverseIndex",
-      projectionType: "ALL",
-      readCapacity: 10,
-      writeCapacity: 10,
-    },
-  ],
-  readCapacity: 20,
-  writeCapacity: 20,
-  billingMode: "PROVISIONED",
-  tags: {
-    Environment: "production",
-    Name: "CEMDEV",
-  },
-  ttl: {
-    attributeName: "ttl",
-    enabled: false,
-  },
-});
+const buildDynamoTable = (table: string, environment: string) => {
+  const tableName = table.toUpperCase();
 
-const dynamoDbTableDev = new aws.dynamodb.Table("CEMDEV-DEV", {
-  name: "CEMDEV-DEV",
-  attributes: [
+  return new aws.dynamodb.Table(
+    `${tableName}${environment === "production" ? "" : "-DEV"}`,
     {
-      name: "PK",
-      type: "S",
-    },
-    {
-      name: "SK",
-      type: "S",
-    },
-  ],
-  hashKey: "PK",
-  rangeKey: "SK",
-  globalSecondaryIndexes: [
-    {
-      hashKey: "SK",
-      rangeKey: "PK",
-      name: "ReverseIndex",
-      projectionType: "ALL",
-      readCapacity: 10,
-      writeCapacity: 10,
-    },
-  ],
-  readCapacity: 20,
-  writeCapacity: 20,
-  billingMode: "PROVISIONED",
-  tags: {
-    Environment: "production",
-    Name: "CEMDEV",
-  },
-  ttl: {
-    attributeName: "ttl",
-    enabled: false,
-  },
-});
+      name: tableName,
+      attributes: [
+        {
+          name: "PK",
+          type: "S",
+        },
+        {
+          name: "SK",
+          type: "S",
+        },
+      ],
+      hashKey: "PK",
+      rangeKey: "SK",
+      globalSecondaryIndexes: [
+        {
+          hashKey: "SK",
+          rangeKey: "PK",
+          name: "ReverseIndex",
+          projectionType: "ALL",
+          readCapacity: 1,
+          writeCapacity: 1,
+        },
+      ],
+      readCapacity: 1,
+      writeCapacity: 1,
+      billingMode: "PROVISIONED",
+      tags: {
+        Environment: environment,
+        Name: tableName,
+      },
+      ttl: {
+        attributeName: "ttl",
+        enabled: false,
+      },
+    }
+  );
+};
+
+const dynamoDbTableProd = buildDynamoTable("CEMDEV", "production");
+const dynamoDbTableDev = buildDynamoTable("CEMDEV", "development");
 
 export const dynamoDbTableProdArn = dynamoDbTableProd.arn;
 export const dynamoDbTableDevArn = dynamoDbTableDev.arn;
