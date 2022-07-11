1b8daccdeff9b042af3f0c3e68b7d99044a5e952
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2022-07-11 19:56:20 +0200

    fix: dynamodb table configuration

--- apps/infra/index.ts
+++ apps/infra/index.ts
@@ -2,42 +2,40 @@ import * as aws from "@pulumi/aws";
 
 const buildDynamoTable = (table: string, environment: string) => {
   const tableName = table.toUpperCase();
+  const environmentName = environment === "production" ? "" : "-DEV";
 
-  return new aws.dynamodb.Table(
-    `${tableName}${environment === "production" ? "" : "-DEV"}`,
-    {
-      name: tableName,
-      attributes: [
-        {
-          name: "PK",
-          type: "S",
-        },
-        {
-          name: "SK",
-          type: "S",
-        },
-      ],
-      hashKey: "PK",
-      rangeKey: "SK",
-      globalSecondaryIndexes: [
-        {
-          hashKey: "SK",
-          rangeKey: "PK",
-          name: "ReverseIndex",
-          projectionType: "ALL",
-          readCapacity: 1,
-          writeCapacity: 1,
-        },
-      ],
-      readCapacity: 1,
-      writeCapacity: 1,
-      billingMode: "PROVISIONED",
-      tags: {
-        Environment: environment,
-        Name: tableName,
-      }
-    }
-  );
+  return new aws.dynamodb.Table(`${tableName}${environmentName}`, {
+    name: `${tableName}${environmentName}`,
+    attributes: [
+      {
+        name: "PK",
+        type: "S",
+      },
+      {
+        name: "SK",
+        type: "S",
+      },
+    ],
+    hashKey: "PK",
+    rangeKey: "SK",
+    globalSecondaryIndexes: [
+      {
+        hashKey: "SK",
+        rangeKey: "PK",
+        name: "ReverseIndex",
+        projectionType: "ALL",
+        readCapacity: 1,
+        writeCapacity: 1,
+      },
+    ],
+    readCapacity: 1,
+    writeCapacity: 1,
+    billingMode: "PROVISIONED",
+    tags: {
+      Environment: environment,
+      Name: tableName,
+    },
+  });
 };
 
 const dynamoDbTableProd = buildDynamoTable("CEMDEV", "production");
