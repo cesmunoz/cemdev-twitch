4555f972da678e126e14c7c3ff3b0fdbd54b3687
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2023-01-05 22:09:50 +0100

    fix: change port for fly.io

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -9,7 +9,7 @@ import webhooks from './webhooks';
 import twitchBot from './twitchBot';
 
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // const server = fastify({ logger: true });
 
