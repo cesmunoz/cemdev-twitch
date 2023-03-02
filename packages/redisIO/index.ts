880740ba50699407b89241dd1ccb73aa58f6dd94
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2023-03-02 21:12:10 +0100

    fix: move to pnpm and fix packages dynamoIO and redisIO

--- packages/redisIO/index.ts
+++ packages/redisIO/index.ts
@@ -1,4 +1,6 @@
-import Redis, { KeyType, ValueType } from 'ioredis';
+import Redis, { RedisKey, RedisValue } from 'ioredis';
+
+
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,12 +8,12 @@ const { REDIS_URL } = process.env;
 
 type ExpirationType = number | string;
 
-const client = new Redis(REDIS_URL);
+const client = new Redis(REDIS_URL as string);
 
 export default {
-  set: (key: KeyType, value: ValueType) => client.set(key, value),
-  get: (key: KeyType) => client.get(key),
-  delete: (key: KeyType) => client.del(key),
-  setWithExp: (key: KeyType, value: ValueType, exp: ExpirationType) =>
+  set: (key: RedisKey, value: RedisValue) => client.set(key, value),
+  get: (key: RedisKey) => client.get(key),
+  delete: (key: RedisKey) => client.del(key),
+  setWithExp: (key: RedisKey, value: RedisValue, exp: ExpirationType) =>
     client.set(key, value, 'EX', exp),
 };
